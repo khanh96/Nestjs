352bbc03b139063fe044e136791d74dbe8f4af15
Lucian <khanh90296@gmail.com>
2025-05-07 14:44:29 +0700

    fix ts prisma

--- Ecommerce/ecom/generated/prisma/index.js
+++ Ecommerce/ecom/generated/prisma/index.js
@@ -502,11 +502,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://lucian:123456@localhost:5432/ecom_dev?schema=public"
+        "value": null
       }
     }
   },
