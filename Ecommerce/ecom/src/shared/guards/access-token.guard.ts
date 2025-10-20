a91b277b3825ca85b007c70f290c926f21e770e4
Lucian <khanh90296@gmail.com>
2025-10-20 16:26:13 +0700

    refactor guard access token, authentication

--- Ecommerce/ecom/src/shared/guards/access-token.guard.ts
+++ Ecommerce/ecom/src/shared/guards/access-token.guard.ts
@@ -1,33 +1,85 @@
-import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common'
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException, ForbiddenException } from '@nestjs/common'
+import { Request } from 'express'
 import { REQUEST_USER_KEY } from 'src/shared/constants/auth.constant'
+import { HTTPMethod } from 'src/shared/constants/role.constant'
+import { PrismaService } from 'src/shared/services/prisma/prisma.service'
 import { TokenService } from 'src/shared/services/token/token.service'
+import { AccessTokenPayload } from 'src/shared/types/jwt.type'
 
 @Injectable()
 export class AccessTokenGuard implements CanActivate {
-  constructor(private readonly tokenService: TokenService) {}
+  constructor(
+    private readonly tokenService: TokenService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const ctx = context.switchToHttp()
     const request = ctx.getRequest<Request>()
-    const authorizationHeader = request.headers['authorization']
-    const accessToken = typeof authorizationHeader === 'string' ? authorizationHeader.split(' ')[1] : null // Extract the token from the Authorization header
-    // console.log('request=>', accessToken)
 
-    // Check if the token is present
-    if (!accessToken) {
-      throw new UnauthorizedException('Access token is missing')
-    }
+    // Extract and validate the access token
+    const decodedAccessToken = await this.extractAndValidateToken(request)
+
+    // Optionally, Check user permissions or roles if needed
+    await this.validateUserPermission(decodedAccessToken, request)
+    return true
+  }
+
+  private async extractAndValidateToken(request: Request): Promise<AccessTokenPayload> {
+    const accessToken = this.extractAccessTokenFromHeader(request)
+
     // Check if the token is valid
     try {
       const decodedAccessToken = await this.tokenService.verifyAccessToken(accessToken)
       request[REQUEST_USER_KEY] = decodedAccessToken // Attach the user ID to the request object for later use
-      //   console.log('request[REQUEST_USER_KEY]=>', request)
-      return true
+      return decodedAccessToken
     } catch (error) {
       throw new UnauthorizedException({
         message: 'Invalid access token',
         error: error,
       })
+    }
+  }
+
+  private extractAccessTokenFromHeader(request: Request): string {
+    const authorizationHeader = request.headers['authorization']
+    const accessToken = typeof authorizationHeader === 'string' ? authorizationHeader.split(' ')[1] : null // Extract the token from the Authorization header
+
+    // Check if the token is present
+    if (!accessToken) {
+      throw new UnauthorizedException('Access token is missing')
+    }
+
+    return accessToken
+  }
+
+  private async validateUserPermission(decodedAccessToken: AccessTokenPayload, request: Request): Promise<void> {
+    const roleId = decodedAccessToken.roleId
+    const method = request.method as keyof typeof HTTPMethod
+    const path = request.route.path as string
+    const role = await this.prismaService.role
+      .findFirstOrThrow({
+        where: {
+          id: roleId,
+          deletedAt: null,
+        },
+        include: {
+          permissions: {
+            where: {
+              deletedAt: null,
+              path: path,
+              method: method,
+            },
+          },
+        },
+      })
+      .catch(() => {
+        throw new ForbiddenException()
+      })
+
+    const canAccess = role.permissions.length > 0
+    if (!canAccess) {
+      throw new ForbiddenException()
     }
   }
 }
--- Ecommerce/ecom/src/shared/guards/authentication.guard.ts
+++ Ecommerce/ecom/src/shared/guards/authentication.guard.ts
@@ -1,4 +1,4 @@
-import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common'
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException, HttpException } from '@nestjs/common'
 import { Reflector } from '@nestjs/core'
 import { AuthType, AuthTypeDecoratorPayload, ConditionGuard } from 'src/shared/constants/auth.constant'
 import { AUTH_TYPE_KEY } from 'src/shared/decorators/auth.decorator'
@@ -22,41 +22,60 @@ export class AuthenticationGuard implements CanActivate {
   }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const authTypeValue = this.reflector.getAllAndOverride<AuthTypeDecoratorPayload | undefined>(AUTH_TYPE_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]) ?? { authTypes: [AuthType.Bearer], options: { condition: ConditionGuard.AND } }
+    const authTypeValue = this.getAuthTypeValue(context)
     // console.log('authTypeValue', authTypeValue)
     // Từ authTypeValue.authTypes lấy ra các guard tương ứng
     const guards = authTypeValue.authTypes.map((authType) => {
       return this.authTypeGuardMap[authType]
     })
-    let error = new UnauthorizedException()
-    if (authTypeValue.options.condition === ConditionGuard.OR) {
-      for (const instance of guards) {
-        // Sủ dụng Promise.resolve để bắt lỗi. Cho dù guard nào trả về lỗi thì vẫn tiếp tục kiểm tra các guard còn lại
-        const canActivate = await Promise.resolve(instance.canActivate(context)).catch((err) => {
-          error = err
-          console.log('error1', error)
-          return false
-        })
 
-        if (canActivate) {
-          return true
-        }
-      }
-      throw error
-    } else {
-      for (const instance of guards) {
-        const canActivate = await Promise.resolve(instance.canActivate(context)).catch((err) => {
-          error = err
-          return false
-        })
-        if (!canActivate) {
+    const authResult =
+      authTypeValue.options.condition === ConditionGuard.OR
+        ? this.handleOrCondition(guards, context)
+        : this.handleAndCondition(guards, context)
+    return authResult
+  }
+
+  private getAuthTypeValue(context: ExecutionContext): AuthTypeDecoratorPayload {
+    const authTypeValue = this.reflector.getAllAndOverride<AuthTypeDecoratorPayload | undefined>(AUTH_TYPE_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]) ?? { authTypes: [AuthType.Bearer], options: { condition: ConditionGuard.AND } }
+    return authTypeValue
+  }
+
+  private async handleAndCondition(guards: CanActivate[], context: ExecutionContext) {
+    // Duyệt qua hết các guard, nếu mọi guard pass thì return true
+    for (const instance of guards) {
+      try {
+        if (!(await instance.canActivate(context))) {
           throw new UnauthorizedException()
         }
+      } catch (error) {
+        if (error instanceof HttpException) {
+          throw error
+        }
+        throw new UnauthorizedException()
       }
-      return true
     }
+    return true
+  }
+
+  private async handleOrCondition(guards: CanActivate[], context: ExecutionContext) {
+    let lastError: any = null
+    // Duyệt qua hết các guard, nếu có 1 guard pass thì return true
+    for (const instance of guards) {
+      try {
+        if (await instance.canActivate(context)) {
+          return true
+        }
+      } catch (error) {
+        lastError = error
+      }
+    }
+    if (lastError instanceof HttpException) {
+      throw lastError
+    }
+    throw new UnauthorizedException()
   }
 }
