099747799ef20511bc91ba1c9747872851ea63eb
Lucian <khanh90296@gmail.com>
2026-01-07 10:22:03 +0700

    Bai 177 Helmet

--- Ecommerce/ecom/src/main.ts
+++ Ecommerce/ecom/src/main.ts
@@ -1,12 +1,16 @@
+import helmet from 'helmet'
+import { patchNestJsSwagger } from 'nestjs-zod'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { WebsocketAdapter } from 'src/websockets/websocket.adapter'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
-import { patchNestJsSwagger } from 'nestjs-zod'
 import { NestExpressApplication } from '@nestjs/platform-express'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
+
+  app.use(helmet())
+
   // Trust requests from loopback address (e.g., proxies like Nginx running on the same machine)
   app.set('trust proxy', 'loopback') // Trust requests from the loopback address
   // Enable CORS
